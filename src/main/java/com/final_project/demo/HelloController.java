39ea68196372deb422caff2ed066bde7d203966e
Jui1200 <50120benson@gmail.com>
2024-09-09 11:15:05 +0800

    nothing

--- src/main/java/com/final_project/demo/HelloController.java
+++ src/main/java/com/final_project/demo/HelloController.java
@@ -3,6 +3,8 @@ package com.final_project.demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.final_project.model.Fund;
+
 @RestController
 public class HelloController {
 	@RequestMapping(value = "/")
@@ -13,5 +15,12 @@ public class HelloController {
 	@RequestMapping(value = "/hello")
 	public String hello() {
 		return "Hello World!!";
+	}
+	
+	@RequestMapping(value = "/json")
+	public Fund id() {
+		Fund fund = new Fund();
+		fund.setFundID(12);
+		return fund;
 	}
 }
